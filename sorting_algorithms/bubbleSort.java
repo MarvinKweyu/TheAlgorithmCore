3fb785e1a6a34116cbd7baa51ff44ca2319bff8a
Marvin Kweyu <mkweyu1@gmail.com>
2022-10-12 13:37:16 +0300

    chore: add bubble sort algorithm

---
+++ sorting_algorithms/bubbleSort.java
@@ -0,0 +1,36 @@
+// The function below
+
+/*
+ * The function below is an implementation of the bubble sort algorithm
+ * Contributors: Marvin Kweyu(twitter: @marvinus_j)
+ **/
+
+public class bubbleSort {
+    public static void main(String[] args) {
+        int[] nums = { 99, -10, 100123, 18, -978,
+                5623, 463, -9, 287, 49 };
+
+        sortingAlgorithm(nums);
+
+        for (int item : nums) {
+            System.out.println("The item is: " + item);
+        }
+
+    }
+
+    static void sortingAlgorithm(int[] unsortedArray) {
+        // this is the bubble sort
+
+        for (int i = 1; i < unsortedArray.length; i++) {
+            for (int j = unsortedArray.length - 1; j >= i; j--) {
+                if (unsortedArray[j - 1] > unsortedArray[j]) {
+                    // exchange/ swap occurs
+                    int item = unsortedArray[j - 1];
+                    unsortedArray[j - 1] = unsortedArray[j];
+                    unsortedArray[j] = item;
+                }
+            }
+        }
+
+    }
+}
